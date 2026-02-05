560e41f8fd091d8c729f882f3293d312e06b8c05
kehceurope-png <kehc.europe@gmail.com>
2026-02-05 22:02:39 +0100

    Create app,.py

---
+++ app,.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="유럽직할지방회 임원 시스템")
+
+st.title("🖥️ 유럽직할지방회 임원 행정 시스템")
+st.subheader("환영합니다, 신형채 목사님!")
+st.write("현재 시스템 구축 중입니다. 곧 구글 시트와 연동됩니다.")
+
+st.info("이 화면이 보이면 웹 호스팅 성공입니다!")
